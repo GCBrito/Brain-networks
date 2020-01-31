ed73e1168a474c9e236da25404115e035088a0f4
gcbrito <gctbrito@gmail.com>
2020-01-31 16:43:28 -0300

    Reworked data folder structure

--- R/compel.R
+++
@@ -1,7 +0,0 @@
-#Run scritp
-#
-   # data_prepare()
-    # corr_matrix()
-     #ntw_graph()
-     #ntw_metrics()
-#  suvrmeans()
--- R/ntw_graph.R
+++
@@ -1,9 +0,0 @@
-# Graph creation
-
-ntw_graph <- function() {
-
-    lapply(corr_matrices, function(x) {
-        graph.adjacency(x$r,mode = "undirected", diag = FALSE, weighted =TRUE)}
-    ) %>%
-        assign("ntw",., pos=1)
-}
